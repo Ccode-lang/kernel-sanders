d600ca937b40d9a021a6f59c431b06746acd9d09
BuildTools <unconfigured@null.spigotmc.org>
2023-04-19 19:17:19 -0700

    Simple test terminal

--- main.c
+++ main.c
@@ -3,17 +3,80 @@
 #include "memory.h"
 #include "filesystem.h"
 
+SIMPLE_INPUT_INTERFACE *CI;
+
+int ReadLine(CHAR16 *buf, int l)
+{
+    int i = 0;
+    EFI_INPUT_KEY key;
+    while (1)
+    {
+        if (!uefi_call_wrapper(CI->ReadKeyStroke, 2, CI, &key))
+        {
+            if (key.UnicodeChar == CHAR_LINEFEED || key.UnicodeChar == CHAR_CARRIAGE_RETURN)
+            {
+                Print(L"\n");
+                break;
+            }
+            else if (key.UnicodeChar == CHAR_BACKSPACE || key.ScanCode == SCAN_DELETE)
+            {
+                if (i)
+                    i--;
+                buf[i] = 0;
+                continue;
+            }
+            else if (key.ScanCode == SCAN_ESC)
+            {
+                buf[0] = 0;
+                return 0;
+            }
+            else if (key.UnicodeChar && i < l - 1)
+            {
+                CHAR16 tmp[2];
+                tmp[0] = key.UnicodeChar;
+                tmp[1] = 0;
+                Print(tmp);
+                buf[i++] = key.UnicodeChar;
+                buf[i] = 0;
+            }
+        }
+    }
+    return i;
+}
+
 EFI_STATUS
 EFIAPI
 efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
 {
     InitializeLib(ImageHandle, SystemTable);
+
     uefi_call_wrapper(SystemTable->ConOut->ClearScreen, 1, SystemTable->ConOut);
     Print(L"Hello, world!\n");
     EFI_FILE_HANDLE Volume = GetVolume(ImageHandle);
     CHAR8 *fileContent = (CHAR8 *)ReadFileTooBuffer(L"hello.txt", Volume);
     AsciiPrint(fileContent);
     uefi_free(fileContent);
+
+    CI = SystemTable->ConIn;
+
+    EFI_INPUT_KEY Key;
+    CHAR16 CommandBuffer[256] = L"";
+
+    Print(L">");
+
+    while (1)
+    {
+        ReadLine(CommandBuffer, 256);
+        if (StrCmp(CommandBuffer, L"!test\0") == 0)
+        {
+            Print(L"Test\n");
+        }
+        else
+        {
+            Print(L"Unknown command.\n");
+        }
+        Print(L">");
+    }
 
     while (1)
     {
